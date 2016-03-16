12b52ca6f7750e15048a05cd3701e454fa0fc05b
luckyIce <641055890@qq.com>
2016-03-16 17:13:57 +0800

    longestConsecutive in 5

--- 5/main.cpp
+++ 5/main.cpp
@@ -6,5 +6,33 @@
  ************************************************************************/
 
 #include<iostream>
+#include<map>
+#include<vector>
 using namespace std;
+int longestConsecutive(const vector<int>& nums){
+    unordered_map<int,bool> used;
+    for(auto i:nums){
+        used[i]=false;
+    }
+    int longest=0;
+    for(auto i:nums){
+        if(used[i]){
+            continue;
+        }
+        else{
+            int length=1;
+            used[i]=true;
+            for(int j=i+1;used.find(j)!=used.end();j++){
+                length++;
+                used[j]=true;
+            }
+            for(int j=i-1;used.find(j)!=used.end();j--){
+                length++;
+                used[j]=true;
+            }
+            longest=max(longest,length);
+        }
+    }
+    return longest;
+}
 
