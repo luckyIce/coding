241ef9827a67039ceab363f9397b4a6898de9a1e
luckyIce <641055890@qq.com>
2016-03-16 17:39:03 +0800

    return two numbers that add up to a target number in 6

--- 6/main.cpp
+++ 6/main.cpp
@@ -6,5 +6,23 @@
  ************************************************************************/
 
 #include<iostream>
+#include<vector>
+#include<map>
 using namespace std;
+vector<int> twoSum(vector<int>& nums,int target){
+    unordered_map<int,int> mapping;
+    for(int i=0;i<nums.size();i++){
+        mapping[nums[i]]=i;
+    }
+    vector<int> result;
+    for(int i=0;i<nums.size();i++){
+        const int gap=target-nums[i];
+        if(mapping.find(gap)!=mapping.end()&&mapping.find(gap)!=mapping[nums[i]]){
+            result.push(min(mapping.find(gap),mapping[nums[i]]));
+            result.push(max(mapping.find(gap),mapping[nums[i]]));
+            break;
+        }
+    }
+    return result;
+}
 
