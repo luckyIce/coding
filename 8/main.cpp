941a966bc0842989dbe32a95e70fb6ddc26d5cb0
luckyIce <641055890@qq.com>
2016-03-16 21:02:34 +0800

    remove element in 8

--- 8/main.cpp
+++ 8/main.cpp
@@ -6,5 +6,16 @@
  ************************************************************************/
 
 #include<iostream>
+#include<vector>
 using namespace std;
+int removeElement(vector<int>& nums,int target){
+    int index=0;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i]!=target){
+            nums[index]=nums[i];
+            ++index;
+        }
+    }
+    return index;
+}
 
