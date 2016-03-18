f1dbe36ac7ec25b9a93ce85f8896ca58ead194eb
luckyIce <641055890@qq.com>
2016-03-18 16:39:51 +0800

    return the kth permutation sequence in 10

--- 10/main.cpp
+++ 10/main.cpp
@@ -21,24 +21,28 @@ string getPermutation(int n,int k){
     int remains=n;//remains stands for how many number left
     int score=k-1;//score stands for the location of the item which is the result
     for(int i=0;i<n;i++){
-        int a=score/fac(remains-1)+1;
+        int a=score/fac(remains-1)+1;//a is the rank in the remaining numbers
         s[i]+=a;
         remains=score%fac(remains-1);
         --remains;
     }
+    bool used[n+1];//used shows whether a item can be used or not
+    //init used
+    for(int i=0;i<n+1;i++){
+        used[i]=false;
+    }
     for(int i=1;i<n;i++){
-        int result=s[i]-'0';//result stans for the true number which should locate here
-        bool conflict=true;
-        while(conflict){
-            for(int j=0;j<i;j++){
-                if(s[j]==result){
-                    result++;
-                    break;
+        int result=s[i]-'0';// the final result is the rank in the whole numbers
+        int count=0;
+        for(int j=0;j<n+1;j++){
+            if(used[j]==false){
+                ++count;
+                if(count==result){
+                    result=j;
                 }
             }
-            s[i]=result;
-            conflict=false;
         }
+        s[i]=result+'0';
     }
     return s;
 }
