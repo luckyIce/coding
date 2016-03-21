28761292b088591283ff728fb8419933448be9d1
luckyIce <641055890@qq.com>
2016-03-21 17:19:15 +0800

    how many ways to get to stairs n in 15

--- 15/main.cpp
+++ 15/main.cpp
@@ -6,5 +6,23 @@
  ************************************************************************/
 
 #include<iostream>
+#include<vector>
 using namespace std;
+int climbStairs(int n){
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+    int oneStepLess=1;
+    int twoStepLess=0;
+    int curr=0;
+    for(int i=2;i<=n;i++){
+        curr+=oneStepLess+twoStepLess;
+        twoStepLess=oneStepLess;
+        oneStepLess=curr;
+    }
+    return curr;
+}
 
