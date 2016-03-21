51874833f9c7b38e053592476e23904e9702c351
luckyIce <641055890@qq.com>
2016-03-21 17:06:35 +0800

    big number in 14

--- 14/main.cpp
+++ 14/main.cpp
@@ -6,5 +6,22 @@
  ************************************************************************/
 
 #include<iostream>
+#include<vector>
 using namespace std;
+void add(vector<int>& digits int digit){
+    int carry=digit;
+    for(auto it=digits.rbegin();it!=digits.rend();it++){
+        *it+=carry;
+        carry=*it/10;
+        *it%=10;
+    }
+    if(carry>0){
+        digits.push_front(carry);
+    }
+
+}
+vector<int> plusOne(vector<int>& digits){
+    add(digits,1);
+    return digits;
+}
 
