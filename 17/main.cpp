57cb0ce2add1e1776492556ff6de9889a4914d99
luckyIce <641055890@qq.com>
2016-03-24 16:33:22 +0800

    setZero in 17

--- 17/main.cpp
+++ 17/main.cpp
@@ -6,5 +6,28 @@
  ************************************************************************/
 
 #include<iostream>
+#include<vector>
 using namespace std;
+void setZeros(vector<vector<int> >& matrix){
+    //record which rows and cols should be set zero
+    const int m=matrix.size();
+    const int n=matrix[0].size();
+    vector<bool> row(m,false);
+    vector<bool> col(n,false);
+    for(int i=0;i<m;i++){
+        for(int j=0;j<m;j++){
+            if(matrix[i][j]==0){
+                row[i]=true;
+                col[j]=true;
+            }
+        }
+    }
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(row[i]||col[j]){
+                matrix[i][j]=0;
+            }
+        }
+    }
+}
 
