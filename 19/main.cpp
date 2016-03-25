c6940ddc084f7e12d29f352210850f94b31c9bd6
luckyIce <641055890@qq.com>
2016-03-25 17:14:25 +0800

    candy in 19 not correct because of the exist of equal value

--- 19/main.cpp
+++ 19/main.cpp
@@ -6,5 +6,76 @@
  ************************************************************************/
 
 #include<iostream>
+#include<vector>
 using namespace std;
+bool find(vector<int> &indexs,int index){
+    const int n=indexs.size();
+    int begin=0;
+    int end=n;
+    int mid=(begin+end)/2;
+    while(begin!=end){
+        if(indexs[mid]==index){
+            return true;
+        }
+        if(indexs[mid]<index){
+            begin=mid+1;
+        }
+        else{
+            end=mid;
+        }
+    }
+    return false;
+}
+int candy(vector<int> &ratings){
+    const int n=ratings.size();
+    //each one must be in one of these classes
+    //ones in first class are those whose rating are less than the both side of their neiborhood so they should be given one candy
+    //ones  in second class are those whose rating are betwen their neiborhood so they should be given one more than their junior rating neiborhood
+    //one in third class are those whose rating are more than the both side of their neiborhood so they should be given one more than their senior rating neiborhood
+    //decide each item belonging to which class
+    vector<int> firstclass;//store their index
+    vector<int> secondclass;//store their index
+    vector<int> thirdclass;//store their index
+    for(int i=1;i<n-1;i++){
+        if(ratings[i]<ratings[i-1]&&ratings[i]<ratings[i+1]){
+            firstclass.push_back(i);
+
+        }
+        else if(ratings[i]>ratings[i-1]&&ratings[i]>ratings[i+1])
+        {
+            thirdclass.push_back(i);
+        }
+        else if(ratings[i]==ratings[i-1]||ratings[i]==ratings[i+1])
+        {
+            thirdclass.push_back(i);
+        }
+        else{
+
+            secondclass.push_back(i);
+        }
+
+    }
+    int sum=n;//how many candy should be given
+    for(auto it=firstclass.begin();it!=firstclass.end();it++){
+        //left side
+        int left=*it-1;
+        int extra=0;
+        while(find(secondclass,left)||find(firstclass,left)){
+            if(find(secondclass,left)){
+                ++extra;
+                sum+=extra;
+            }
+            left--;
+        }
+        int right=*it+1;
+        while(find(secondclass,right)||find(firstclass,right)){
+            if(find(secondclass,right)){
+                ++extra;
+                sum+=extra;
+            }
+            right--;
+        }
+    }
+
+}
 
