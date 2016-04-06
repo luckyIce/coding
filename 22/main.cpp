e78feb83ab642c6c08bee47a9a6ab72c1b7402cd
luckyIce <641055890@qq.com>
2016-04-06 16:01:38 +0800

    reverseBetween

--- 22/main.cpp
+++ 22/main.cpp
@@ -7,4 +7,28 @@
 
 #include<iostream>
 using namespace std;
-
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode(int x):val(x),next(nullptr)
+};
+ListNode* reverseBetween(ListNode *head,int m,int n){
+    //头插法
+    ListNode *dummy(-1);
+    dummy.next=head;
+    ListNode* prev=&dummy;
+    for(int i=0;i<m-1;i++){
+        prev=prev->next;
+    }
+    //prev代表m元素前的元素
+    ListNode *cur=prev->next;
+    ListNode *post=cur->next;
+    for(int i=m;i<n;i++){
+        prev->next=post;
+        cur->next=prev->next;
+        prev->next=cur;
+        cur=cur->next;
+        post=post->next;
+    }
+    return dummy;
+}
