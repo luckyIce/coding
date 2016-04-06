9cdf5e2ea0d70076aa0401a7cfe476cdb2782fc9
luckyIce <641055890@qq.com>
2016-04-06 16:35:31 +0800

    removeDuplicates in sorted list in 24

--- 24/main.cpp
+++ 24/main.cpp
@@ -7,4 +7,23 @@
 
 #include<iostream>
 using namespace std;
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode(int x):val(x),next(nullptr)
+};
+void removeDuplicates(ListNode *head){
+    ListNode *tail=head;
+    ListNode *cur=head->next;
+    while(cur){
+        if(cur->val==tail->val){
+            tail->next=cur->next;
+            delete cur;
+        }
+        else{
+            tail=cur;
+        }
+        cur=tail->next;
+    }
+}
 
