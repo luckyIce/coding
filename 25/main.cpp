7bb5f0b8acf9e20f207a2729ccbe36848defe737
luckyIce <641055890@qq.com>
2016-04-06 16:58:32 +0800

    deleteDuplicates in 25

--- 25/main.cpp
+++ 25/main.cpp
@@ -7,4 +7,37 @@
 
 #include<iostream>
 using namespace std;
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode(int x):val(x),next(nullptr)
+};
+ListNode *deleteDuplicates(ListNode *head){
+    if(head==nullptr) return head;
+    ListNode dummy(-1);
+    dummy.next=head;
+    ListNode *cur=head;
+    ListNode *prev=&dummy;
+    ListNode *post=nullptr;
+    while(cur){
+        //decide whether current node is a duplicated one
+        if(post&&post->val!=cur->val){//if not test next item
+            cur=post;
+            post=post->next;
+            prev=cur;
+        }
+        else{//remove duplicate
+            int value=cur->val;
+            while(cur&&cur->val==value){
+                prev->next=cur->next;
+                delete cur;
+                cur=prev->next;
+            }
+        }
 
+    }
+    if(prev){
+        prev->next=nullptr;
+    }
+
+}
