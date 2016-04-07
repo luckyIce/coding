fada758608cb4ea06f73dfc019a2d345930ab0e8
luckyIce <641055890@qq.com>
2016-04-07 16:11:16 +0800

    removeFromEnd in 27

--- 27/main.cpp
+++ 27/main.cpp
@@ -7,4 +7,27 @@
 
 #include<iostream>
 using namespace std;
-
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode(int x):val(x),next(nullptr)
+};
+ListNode* removeNthFromEnd(ListNode* head,int n){
+    ListNode dummy(-1);
+    dummy.next=head;
+    ListNode* p=head;
+    ListNode* q=head;
+    int i=0;
+    while (i<n) {
+        q=q->next;
+        i++;
+    }
+    while (q->next) {
+        p=p->next;
+        q=q->next;
+    }
+    ListNode *r=p->next;
+    p->next=r->next;
+    delete r;
+    return dummy.next;
+}
