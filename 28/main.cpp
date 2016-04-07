b8cbcc7fc6d6bbb681089dea674b575a35e990a0
luckyIce <641055890@qq.com>
2016-04-07 16:33:01 +0800

    swapInPairs in 28

--- 28/main.cpp
+++ 28/main.cpp
@@ -7,4 +7,26 @@
 
 #include<iostream>
 using namespace std;
-
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode(int x):val(x),next(nullptr)
+};
+ListNode* swapPairs(ListNode *head){
+    ListNode dummy(-1);
+    dummy.next=head;
+    ListNode *h=&dummy;
+    ListNode *first=h->next;
+    first&&ListNode *second=first->next;
+    while(first&&second){
+        first->next=second->next;//delete second item from list
+        //头插法插入second
+        second->next=h->next;
+        h->next=second;
+        //update h first second
+        h=first;
+        first=h->next;
+        first&&ListNode *second=first->next;
+    }
+    return dummy.next;
+}
