2d990dceb8dd058aa890ff6f0a1558335ac1b965
luckyIce <641055890@qq.com>
2016-04-07 17:17:13 +0800

    hasCycle in 29

--- 29/main.cpp
+++ 29/main.cpp
@@ -7,4 +7,20 @@
 
 #include<iostream>
 using namespace std;
-
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode(int x):val(x),next(nullptr)
+};
+bool hasCycle(ListNode *head){
+    ListNode *fast=head;
+    ListNode *slow=head;
+    while(fast&&fast->next){
+        fast=fast->next->next;
+        slow=slow->next;
+        if(slow==fast){
+            return false;
+        }
+    }
+    return true;
+}
