1082db00c321870ee2962ac355be2e75e651e2d7
luckyIce <641055890@qq.com>
2016-04-08 17:57:26 +0800

    find the first occurence of needle in haystack in 31

--- 31/main.cpp
+++ 31/main.cpp
@@ -7,4 +7,21 @@
 
 #include<iostream>
 using namespace std;
+int strStr(const string& haystack,const string& needle){
+    const int N=haystack.size();
+    const int n=needle.size();
+    for(int i=0;i<=N-n;i++){
+        //i stands for the begin index of the match
+        int j=i;//j stands for current char in haystack
+        int k=0;//k stands for current char in needle
+        while(k<n&&haystack[j]==needle[k]){
+            j++;
+            k++;
 
+        }
+        if(k=n){//find match
+            return i;
+        }
+    }
+    return -1;//no match
+}
