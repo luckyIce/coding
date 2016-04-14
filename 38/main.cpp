74062509cecc3b1997197ea20048ba75ec834889
luckyIce <641055890@qq.com>
2016-04-14 20:43:18 +0800

    Anagram sudocode in 38

--- 38/main.cpp
+++ 38/main.cpp
@@ -6,5 +6,32 @@
  ************************************************************************/
 
 #include<iostream>
+#include<string>
+#include<vector>
 using namespace std;
+string baseOfStr(const string &s){
+    string result=s;
+    sort(s.begin(),s.end());
+    return result;
+}
+map<string,vector<string> groupsOfStrs(const vector<string> &strs){
+    map groups;
+    for(auto element:strs){
+        string key=baseOfStr(element);
+        if(!find(key,groups)){
+            groups.add(key,vector<string>());
+        }
+        groups[key].push_back(element);
+    }
+    return groups;
+}
+void outPutStrInGroup(map<string,vector<string> groups){
+    for(auto element:groups){
+        for(auto str:element){
+            cout<<str<<" ";
+        }
+        cout<<endl;
+    }
+}
+
 
