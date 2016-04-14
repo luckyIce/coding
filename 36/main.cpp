78b2609920ba5eaae4fa15adca122f4b08f42c87
luckyIce <641055890@qq.com>
2016-04-14 19:30:07 +0800

    longestCommonPrefix in 36

--- 36/main.cpp
+++ 36/main.cpp
@@ -6,5 +6,42 @@
  ************************************************************************/
 
 #include<iostream>
+#include<vector>
+#include<string>
 using namespace std;
+// string longestCommonPrefix(const vector<const string> &strs){
+    // if(strs.empty()){
+        // return "";
+    // }
+    // for(int index=0;index<strs[0].size();index++){
+        // for(int i=1;i<strs.size();i++){
+            // if(index==strs[i].size()||strs[i][index]!=strs[0][index]){
+                // return strs[0].substr(0,index);
+            // }
+        // }
+    // }
+    // return strs[0];
+// }
+string longestCommonPrefix(const vector<const string> &strs){
+    if(strs.empty()){
+        return "";
+    }
+    //find the first i strings's longestCommonPrefix 循环不定式
+    int len=strs[0].size();//len stands for the length of longestCommonPrefix of the first strings
+    for(int i=1;i<strs.size();i++){
+        for(int j=0;j<len;j++){//the first j chars must be the same in two strings
+            if(strs[i][j]!=strs[0][j]){
+                len=j;
+                break;
+            }
+        }
+    }
+    return strs[0].substr(0,len);
 
+}
+int main(){
+    vector<const string> strs={"lucky","lumm"};
+    vector<int > i={1,2,3};
+    cout<<longestCommonPrefix(strs)<<endl;
+    return 0;
+}
