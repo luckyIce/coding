584f38cb0544c71f36848173ea06207198d9920a
luckyIce <641055890@qq.com>
2016-04-15 15:33:22 +0800

    learn stringstream in 39

--- 39/main.cpp
+++ 39/main.cpp
@@ -6,5 +6,24 @@
  ************************************************************************/
 
 #include<iostream>
+#include<string>
+#include<stringstream>
 using namespace std;
-
+string countAndSay(int n){
+    string s("1");
+    while(--n){//n stands for the index in unresoved sequence,index starts from one
+        s=getNext(s);
+    }
+    return s;
+}
+string getNext(const string &s){
+    stringstream ss;
+    //count how many same num in the prefix of s
+    for(auto it=s.cbegin();it!=s.cend();){
+        //find the char which is not the same char in *it
+        auto last=s.find_if_not(it,s.cend());
+        ss<<distance(last-it)<<*it;//put in the number and char
+        it=last;
+    }
+    return ss.str();
+}
