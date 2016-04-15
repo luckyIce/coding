6231ef6b40278924c79d5b05285bc12873e89b81
luckyIce <641055890@qq.com>
2016-04-15 16:13:00 +0800

    simplifyPath in 40

--- 40/main.cpp
+++ 40/main.cpp
@@ -6,5 +6,31 @@
  ************************************************************************/
 
 #include<iostream>
+#include<string>
 using namespace std;
+string simplifyPath(const string &s){
+    vector<string> strs;//store sub dir part
+    //find next item between '/'
+    for(auto it=s.cbegin();it!=s.cend();){
+        ++it;//skip '/'
+        auto last=find_if(it,s.cend(),'/');
+        string str=string(it,last);
+        if(!str.empty()&&str!='.'){
+            if(str==".."){
+                strs.empty()||strs.pop_back();
+            }
+            else{
+                strs.push_back(str);
+            }
+        }
 
+    }
+    stringstream ss;
+    if(str.empty()){
+        return "/";
+    }
+    for(auto it=strs.cbegin();it!=strs.cend();it++){
+        ss<<"/"<<*it;
+    }
+    return ss.str();
+}
