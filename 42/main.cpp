47cd7125094d3d70302012d8f32712bbb58816a3
luckyIce <641055890@qq.com>
2016-04-22 13:47:14 +0800

    match  in 42

--- 42/main.cpp
+++ 42/main.cpp
@@ -6,5 +6,35 @@
  ************************************************************************/
 
 #include<iostream>
+#include<string>
+#include<stack>
 using namespace std;
+bool isValid(const string& s){
+    stack<char> match;
+    string left="([{";
+    string right=")]}";
+    for(auto c:s){
+        if(left.find(c)!=string::npos){
+            match.push(c);
+        }
+        else{
+            if(match.empty()||right[left.find(match.top())]!=c){
+                return false;
+            }
+            match.pop();
+        }
+    }
+    return match.empty();
+}
+int main(){
+    string s="((()))";
+    if(isValid(s)){
+        cout<<"match"<<endl;
+    }
+    else{
+        cout<<" not match"<<endl;
+    }
+    return 0;
+
+}
 
