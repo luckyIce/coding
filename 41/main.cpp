028ee8bc4d6465eb7819040faffeb1b8a392e5b1
luckyIce <641055890@qq.com>
2016-04-22 13:32:45 +0800

    lastWord in 41

--- 41/main.cpp
+++ 41/main.cpp
@@ -6,5 +6,16 @@
  ************************************************************************/
 
 #include<iostream>
+#include<algorithm>
+#include<string>
 using namespace std;
-
+string lastWord(const string& s){
+    auto first=find_if(s.rbegin(),s.rend(),::isalpha);
+    auto end=find_if_not(first,s.rend(),::isalpha);
+    return distance(first,end);
+}
+int main(){
+    string s="lkua cky";
+    cout<<lastWord(s);
+    return 0;
+}
