18a7981ce65365ccd4630af22ad50a5a9c4b15f1
luckyIce <641055890@qq.com>
2016-04-22 18:46:20 +0800

    removeDuplicates in 43

--- 43/main.cpp
+++ 43/main.cpp
@@ -6,5 +6,21 @@
  ************************************************************************/
 
 #include<iostream>
+#include<vector>
 using namespace std;
+int removeDuplicates(vector<int> nums){
+    int index=0;
+    for(auto num:nums){
+        if(index==0||num!=nums[index-1]){
+            nums[index]=num;
+            index++;
+        }
+    }
+    return index;
+}
+int main(){
+    vector<int> nums={1,1,2};
+    cout<<removeDuplicates(nums);
+    return 0;
+}
 
