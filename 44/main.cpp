ed29f711f91183fdf5dfda19839786988b974979
luckyIce <641055890@qq.com>
2016-04-22 20:49:18 +0800

    search in rotated sorted array in 44

--- 44/main.cpp
+++ 44/main.cpp
@@ -17,18 +17,28 @@ int binarySearch(const vector<int> &nums,int value){
         if(nums[mid]==value){
             return mid+1;//index start from 0
         }
-        else if(nums[mid]<value){
-            begin=mid+1;
+        if(nums[begin]<=nums[mid]){//the first half are sorted
+            if(nums[mid]>value&&nums[begin]<=value){//value is in the first half
+                end=mid;
+            }
+            else{
+                begin=mid+1;
+            }
         }
         else{
-            end=mid;
+            if(nums[mid+1]<=value&&nums[end-1]>=value){
+                begin=mid+1;
+            }
+            else{
+                end=mid;
+            }
         }
     }
     return -1;
 }
 int main(){
-    vector<int> nums={1,2,3,4,5,6,7};
-    int value=1;
+    vector<int> nums={4,5,6,7,0,1,2};
+    int value=4;
     cout<<binarySearch(nums,value);
 }
 
