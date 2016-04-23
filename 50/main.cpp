e4f7e135cd28a25fce1825bd3045dc4113a8aa9b
luckyIce <641055890@qq.com>
2016-04-23 17:33:57 +0800

    removeTarget in 50

--- 50/main.cpp
+++ 50/main.cpp
@@ -6,5 +6,21 @@
  ************************************************************************/
 
 #include<iostream>
+#include<vector>
 using namespace std;
+int removeTarget(vector<int>& nums,int target){
+    int index=0;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i]!=target){
+            nums[index++]=nums[i];
+        }
+    }
+    return index;
+}
+int main(){
+    vector<int> nums={1,1,3,5,43,3,5,3};
+    int target=3;
+    cout<<removeTarget(nums,target);
+    return 0;
+}
 
