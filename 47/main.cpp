5b615cc824cf3bb62e9560df3be642c3be991ad0
luckyIce <641055890@qq.com>
2016-04-23 10:45:40 +0800

    unordered_map in 47

--- 47/main.cpp
+++ 47/main.cpp
@@ -6,5 +6,36 @@
  ************************************************************************/
 
 #include<iostream>
+#include<vector>
+#include<unordered_map>
 using namespace std;
+int longestConsecutive(const vector<int>& nums){
+    unordered_map<int,bool> used;
+    for(auto num:nums){
+        used[num]=false;
+    }
+    int longest=0;
+    for(auto num:nums){
+        if(used[num]){
+            continue;
+        }
+        used[num]=true;
+        int length=1;
+        for(int j=num+1;used.find(j)!=used.end();++j){
+            used[j]=true;
+            length++;
+        }
+        for(int j=num-1;used.find(j)!=used.end();--j){
+            used[j]=true;
+            length++;
+        }
+        longest=max(longest,length);
+    }
+    return longest;
+}
+int main(){
+    vector<int> nums={100,4,200,1,3,2};
+    cout<<longestConsecutive(nums);
+    return 0;
+}
 
