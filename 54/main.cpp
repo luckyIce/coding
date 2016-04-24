a4d0137cf26b091ec48fe52d3866d949f152581e
luckyIce <641055890@qq.com>
2016-04-24 10:25:39 +0800

    trap in 54

--- 54/main.cpp
+++ 54/main.cpp
@@ -6,5 +6,36 @@
  ************************************************************************/
 
 #include<iostream>
+#include<vector>
 using namespace std;
-
+int trap(const vector<int>& nums){
+    int max=0;
+    int index=0;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i]>max){
+            max=nums[i];
+            index=i;
+        }
+    }
+    int height=0;
+    int total=0;
+    for(int i=0;i<index;i++){
+        if(nums[i]>height){
+            height=nums[i];
+        }
+        total+=height-nums[i];
+    }
+    height=0;
+    for(int i=nums.size()-1;i>index;i--){
+        if(nums[i]>height){
+            height=nums[i];
+        }
+        total+=height-nums[i];
+    }
+    return total;
+}
+int main() {
+   vector<int> nums={0,1,0,2,1,0,1,3,2,1,2,1};
+   cout<<trap(nums);
+    return 0;
+}
