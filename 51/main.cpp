37903c7e920c985a38e84e0e40ced4dc2d361872
luckyIce <641055890@qq.com>
2016-04-24 08:48:09 +0800

    nextPermutation in 51

--- 51/main.cpp
+++ 51/main.cpp
@@ -6,5 +6,26 @@
  ************************************************************************/
 
 #include<iostream>
+#include<vector>
 using namespace std;
+void nextPermutation(vector<int>& nums){
+    auto it=nums.rbegin()+1;
+    for(;it!=nums.rend();it++){//find first item
+        if(*it<*(it-1)){
+            break;
+        }
+    }
+    if(it==nums.rend()){
+        reverse(nums.begin(),nums.end());
+    }
+    auto change=nums.rbegin();
+    for(;*change<=*it;change++);
+    swap(*it,*change);
+    reverse(nums.rbegin(),it);
+}
+int main(){
+    vector<int> nums={6,8,7,4,3,2};
+    nextPermutation(nums);
+    return 0;
+}
 
