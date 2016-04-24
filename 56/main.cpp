ae3964e1159427f9a55247e4cd78797cb9618df5
luckyIce <641055890@qq.com>
2016-04-24 12:06:54 +0800

    bigNumber in 56

--- 56/main.cpp
+++ 56/main.cpp
@@ -6,5 +6,25 @@
  ************************************************************************/
 
 #include<iostream>
+#include<vector>
 using namespace std;
+void addOne(vector<int>&nums, int digit){
+    int carry=digit;
+    int tmp;
+    for(auto it=nums.rbegin();it!=nums.rend()&&carry!=0;it++){
+        tmp=*it+carry;
+        *it=tmp%10;
+        carry=tmp/10;
+    }
+    if(carry!=0){
+        nums.insert(nums.begin(),carry);
+    }
+}
+int main() {
+   vector<int> nums={8,9,9,9};
+   int digit=5;
+   addOne(nums,digit);
+
+    return 0;
+}
 
