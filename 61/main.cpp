0bc84e4d398de0c993185fe47984e0a355a5b46d
luckyIce <641055890@qq.com>
2016-04-25 09:07:07 +0800

    how to use bit operator in 61

--- 61/main.cpp
+++ 61/main.cpp
@@ -6,5 +6,19 @@
  ************************************************************************/
 
 #include<iostream>
+#include<vector>
 using namespace std;
+int singleNumber(const vector<int>& nums){
+    int result=0;
+    for(auto num:nums){
+        result^=num;
+    }
+    return result;
+}
+int main(){
+    vector<int> nums={1,3,4,3,4,5,1};
+    cout<<singleNumber(nums);
+    return 0;
+
+}
 
