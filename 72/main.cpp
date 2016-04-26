354959ebfc08ac38b06051a4eb0fe31b619bb5c0
luckyIce <641055890@qq.com>
2016-04-26 20:20:49 +0800

    hasCycle in 72

--- 72/main.cpp
+++ 72/main.cpp
@@ -6,5 +6,52 @@
  ************************************************************************/
 
 #include<iostream>
+#include<vector>
 using namespace std;
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode(int x):val(x),next(nullptr){}
+    void insert_back(int x){
+        ListNode* node=new ListNode(x);
+        ListNode* p=this->next;
+        if(!p){
+            this->next=node;
+        }
+        else{
+            while(p&&p->next){
+                p=p->next;
+            }
+            p->next=node;
+        }
+    }
+    void print(){
+        ListNode* p=next;
+        while (p) {
+            cout<<p->val;
+            p=p->next;
+        }
+    }
+    void destructor(){
+        auto p=next;
+        while(p){
+            auto q=p;
+            p=p->next;
+            delete q;
+        }
+    }
+};
+bool hasCycle(ListNode* head){
+    auto slow=head;
+    auto fast=head;
+    while(fast&&fast->next){
+        fast=fast->next->next;
+        slow=slow->next;
+        if(slow==fast){
+            return true;
+        }
+    }
+    return false;
+}
+
 
