4460cdbed9e53eb923dafc2967a4b4123f9d2196
luckyIce <641055890@qq.com>
2016-04-26 21:58:44 +0800

    learn transform in 74

--- 74/main.cpp
+++ 74/main.cpp
@@ -6,5 +6,36 @@
  ************************************************************************/
 
 #include<iostream>
+#include<string>
+#include<algorithm>
 using namespace std;
+bool isPalindrome(string s){
+    transform(s.begin(),s.end(),s.begin(),::tolower);
+    auto left=s.begin();
+    auto right=prev(s.end());
+    while(left<right){
+        if(!::isalnum(*left))++left;
+        else if(!::isalnum(*right))--right;
+        else if(*left!=*right){
+            return false;
+        }
+        else{
+            ++left;
+            --right;
+        }
+    }
+    return true;
+}
+int main() {
+   string   s="A man, a plan, a canal: panama" ;
+   if(isPalindrome(s)){
+       cout<<"good"<<endl;
+   }
+   else{
+       cout<<"bad"<<endl;
+   }
+
+
+    return 0;
+}
 
