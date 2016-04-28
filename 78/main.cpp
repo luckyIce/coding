0bffe614a993a471e671318f2d564ce0cdc222d4
luckyIce <641055890@qq.com>
2016-04-28 17:44:33 +0800

    longestPalindrome in 78

--- 78/main.cpp
+++ 78/main.cpp
@@ -6,5 +6,50 @@
  ************************************************************************/
 
 #include<iostream>
+#include<vector>
+#include<string>
 using namespace std;
+string longestPalindrome(const string& s){
+    const int N=s.size();
+    int max_len=1;
+    bool board[N][N];
+    for(int i=0;i<N;i++){
+        board[i][i+1]=true;
+    }
+    for(int len=2;len<=N;len++){
+        for(int i=0;i<N-len+1;i++){
+            if(len==2){
+                if(s[i]==s[i+len-1]){
+                    board[i][i+len]=true;
+                    max_len=len;
+                }
+                else{
+                    board[i][i+len]=false;
+                }
+            }
+            else{
+                if(s[i]==s[i+len-1]&&board[i+1][i+len-1]){
+                    board[i][i+len]=true;
+                    max_len=len;
+
+                }
+                else{
+                    board[i][i+len]=false;
+                }
+            }
+        }
+    }
+    for(int i=0;i<N-max_len+1;i++){
+        if(board[i][i+max_len]){
+            return s.substr(i,max_len);
+        }
+    }
+    return "-1";
+}
+int main() {
+   string s="levcelb";
+   cout<<longestPalindrome(s);
+    return 0;
+}
+
 
