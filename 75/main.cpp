496613f35b9ede4cd75952bebfae0fe80577182d
luckyIce <641055890@qq.com>
2016-04-28 16:34:34 +0800

    implement strStr in 75

--- 75/main.cpp
+++ 75/main.cpp
@@ -6,5 +6,31 @@
  ************************************************************************/
 
 #include<iostream>
+#include<string>
+#include<vector>
 using namespace std;
+int strStr(const string& haystack,const string& needle){
+    if(needle.empty()){
+        return 0;
+    }
+    for(auto i=0;i<haystack.size()-needle.size()+1;i++){
+        int j=0;
+        for(;j<needle.size();j++){
+            if(haystack[i+j]!=needle[j]){
+                break;
+            }
+        }
+        if(j==needle.size()){
+            return i+1;
+        }
+    }
+    return -1;
+}
+int main() {
+   string haystack="haslucky";
+   string needle="luck";
+   cout<<strStr(haystack,needle);
+    return 0;
+}
+
 
