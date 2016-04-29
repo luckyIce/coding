e12b0485dd46f1495d0806224d9af1e697106cd6
luckyIce <641055890@qq.com>
2016-04-29 18:04:45 +0800

    LargestRectangle in 88

--- 88/main.cpp
+++ 88/main.cpp
@@ -6,5 +6,38 @@
  ************************************************************************/
 
 #include<iostream>
+#include<vector>
 using namespace std;
+int leftMost(const vector<int>& nums,int index){
+    int value=nums[index];
+    int i=index;
+    for(;i>=0;i--){
+        if(nums[i]<value){
+            break;
+        }
+    }
+    return i+1;
+}
+int rightMost(const vector<int>& nums,int index){
+    int value=nums[index];
+    int i=index;
+    for(;i<nums.size();i++){
+        if(nums[i]<value){
+            break;
+        }
+    }
+    return i-1;
+}
+int LargestRectangle(const vector<int>& nums){
+    int largest=0;
+    for(int i=0;i<nums.size();i++){
+        largest=max(largest,nums[i]*(rightMost(nums,i)-leftMost(nums,i)+1));
+    }
+    return largest;
+}
+int main() {
+   vector<int> nums={2,1,5,6,2,3};
+   cout<<LargestRectangle(nums)<<endl;
+    return 0;
+}
 
