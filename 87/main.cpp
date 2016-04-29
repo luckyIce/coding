081b5ab5fe24aa43e17cd82f4e7e9de02e670dd1
luckyIce <641055890@qq.com>
2016-04-29 17:11:18 +0800

    longestParen in 87

--- 87/main.cpp
+++ 87/main.cpp
@@ -6,5 +6,38 @@
  ************************************************************************/
 
 #include<iostream>
+#include<stack>
+#include<string>
 using namespace std;
+int longestValidParen(const string& s){
+    size_t length=0;
+    size_t last=-1;//the index of the last right paren
+    stack<int> left;//the index of the left paren
+    for(size_t i=0;i<s.size();i++){
+        if(s[i]=='('){
+            left.push(i);
+        }
+        else{
+            if(left.empty()){
+                last=i;
+            }
+            else{
+                left.pop();
+                if(left.empty()){
+                    length=max(length,i-last);
+                }
+                else{
+                    length=max(length,i-left.top());
+                }
+            }
+        }
+    }
+    return length;
+}
+int main() {
+   string s="())())";
+   cout<<longestValidParen(s)<<endl;
+
+    return 0;
+}
 
