ea3afca5667f24e4ebd410e19d1a80507c82715e
luckyIce <641055890@qq.com>
2016-04-29 11:49:11 +0800

    simplyPath in 84

--- 84/main.cpp
+++ 84/main.cpp
@@ -6,5 +6,38 @@
  ************************************************************************/
 
 #include<iostream>
+#include<string>
+#include<sstream>
+#include<vector>
 using namespace std;
+string simplyPath(const string& s){
+
+    vector<string> parts;
+    for(auto i=s.begin();i!=s.end();){
+        ++i;//i is the index of '/'
+        auto j=find(i,s.end(),'/');
+        string str=string(i,j);
+        if(str.empty()){
+            break;
+        }
+        if(str!="."&&str!=".."){
+            parts.push_back(str);
+        }
+        else if(str==".."){
+            if(!parts.empty())
+                parts.pop_back();
+        }
+        i=j;
+    }
+    stringstream ss;
+    for(auto it=parts.begin();it!=parts.end();it++){
+        ss<<'/'<<*it;
+    }
+    return ss.str();
+}
+int main() {
+   string s="/home/" ;
+   cout<<simplyPath(s)<<endl;
+    return 0;
+}
 
