a09f417c98a1fc185193a26f95036aef5bda9221
luckyIce <641055890@qq.com>
2016-04-29 08:47:47 +0800

    int2Roman in 81

--- 81/main.cpp
+++ 81/main.cpp
@@ -6,5 +6,25 @@
  ************************************************************************/
 
 #include<iostream>
+#include<vector>
+#include<string>
 using namespace std;
+string int2Roman(int num){
+    const int radix[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+    const string symbol[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+    string roman;
+    for(size_t i=0;num>0;i++){
+        int count=num/radix[i];
+        for(;count>0;--count){
+            roman+=symbol[i];
+        }
+        num=num%radix[i];
+    }
+    return roman;
+}
+int main() {
+   int n=1950;
+   cout<<int2Roman(n);
+    return 0;
+}
 
