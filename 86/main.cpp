8934ee5e79106d1c9ea7c50fc851132b66287cbe
luckyIce <641055890@qq.com>
2016-04-29 16:26:11 +0800

    learn map set stack

--- 86/main.cpp
+++ 86/main.cpp
@@ -6,5 +6,45 @@
  ************************************************************************/
 
 #include<iostream>
+#include<unordered_map>
+#include<unordered_set>
+#include<string>
+#include<stack>
 using namespace std;
+bool isValid(const string& s){
+    unordered_map<char,char> paren={ {'(',')'} , {'[',']'} , {'{','}'} };
+    unordered_set<char> left_paren={'(','{','['};
+    stack<char> stk;
+    for(auto c:s){
+        if(left_paren.find(c)!=left_paren.end()){
+            stk.push(c);
+        }
+        else if(!stk.empty()&&paren[stk.top()]==c){
+                stk.pop();
+        }
+        else{
+            return false;
+        }
+    }
+    if(stk.empty()){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+int main() {
+   string s="({}])";
+   if(isValid(s)){
+       cout<<"true"<<endl;
+   }
+   else{
+       cout<<"flase"<<endl;
+
+   }
+    return 0;
+}
+
+
+
 
