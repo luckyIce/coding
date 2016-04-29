a12480b1cd18daba79aeecc3647b28c4303623bb
luckyIce <641055890@qq.com>
2016-04-29 10:48:12 +0800

    countAndSay in 83

--- 83/main.cpp
+++ 83/main.cpp
@@ -6,5 +6,27 @@
  ************************************************************************/
 
 #include<iostream>
+#include<string>
+#include<sstream>
 using namespace std;
+string countAndSay(int n){
+    string s=to_string(n);
+    stringstream ss;
+    for(int i=0;i<s.size();){
+        int j=i+1;
+        int count=1;
+        while(s[j]==s[i]){
+            count++;
+            j++;
+        }
+        ss<<count<<s[i];
+        i=j;
+    }
+    return ss.str();
+}
+int main() {
+   int n=21;
+   cout<<countAndSay(n)<<endl;
+    return 0;
+}
 
