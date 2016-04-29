5ae34de4e518300a69430e6c33e8043d0c54b3b4
luckyIce <641055890@qq.com>
2016-04-29 12:02:34 +0800

    learn find find_if find_if_not

--- 85/main.cpp
+++ 85/main.cpp
@@ -6,5 +6,20 @@
  ************************************************************************/
 
 #include<iostream>
+#include<algorithm>
+#include<string>
 using namespace std;
+string findLastWord(const string& s){
+    auto first=find_if(s.rbegin(),s.rend(),::isalpha);
+    auto last=find_if_not(first,s.rend(),::isalpha);
+    string result(first,last);
+    reverse(result.begin(),result.end());
+    return result;
+}
+int main(){
+    string s="helo world!";
+    cout<<findLastWord(s)<<endl;
+    return 0;
+}
+
 
