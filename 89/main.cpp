792ce8d56506d95077164a8e4bd55089649c5548
luckyIce <641055890@qq.com>
2016-04-29 18:43:52 +0800

    evaluate reverse polish notation in 89

--- 89/main.cpp
+++ 89/main.cpp
@@ -6,5 +6,45 @@
  ************************************************************************/
 
 #include<iostream>
+#include<vector>
+#include<string>
 using namespace std;
+bool isOperator(const string& s){
+    if(s.size()==1&&string("+-*/").find(s)!=string::npos){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+int evalRPN(vector<string>& strs){
+    auto value=strs.back();
+    strs.pop_back();
+    if(isOperator(value)){
+        int y=evalRPN(strs);
+        int x=evalRPN(strs);
+        if(value=="+"){
+            return x+y;
+        }
+        else if(value=="-"){
+            return x-y;
+        }
+        else if(value=="*"){
+            return x*y;
+        }
+        else{
+            return x/y;
+        }
+
+    }
+    else{
+        return stoi(value);
+    }
+}
+int main() {
+   // vector<string> strs={"2","1","+","3","*"};
+   vector<string> strs={"4","13","5","/","+"};
+   cout<<evalRPN(strs)<<endl;
+    return 0;
+}
 
