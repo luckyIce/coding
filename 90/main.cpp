e512ba4a2cff57e1af04a08e13c42d1f1e44b594
luckyIce <641055890@qq.com>
2016-05-04 16:41:18 +0800

    calculate kernel in 90

--- 90/main.cpp
+++ 90/main.cpp
@@ -8,15 +8,35 @@
 #include<iostream>
 #include<cmath>
 using namespace std;
-void calGaussianKernel(float* data,int r){
-    for(int i=0;i<r;i++){
-            data[i]=exp(i-r/2);
+void calGaussianKernel(float* kernel,int size,double sigma){
+    double scale=-0.5/(sigma*sigma);
+    const double PI=3.141592653;
+    double cons=-scale/PI;
+    double sum=0;
+    for(int i=0;i<size;i++){
+        for (int j = 0; j < size; ++j) {
+            int x=i-size/2;
+            int y=j-size/2;
+            kernel[i*size+j]=cons*exp(scale*(x*x+y*y));
+            sum+=kernel[i*size+j];
+        }
+    }
+    for (int i = 0; i < size*size; ++i) {
+        kernel[i]/=sum;
     }
 }
 int main(){
-    int r=3;
-    float* data=new float[r];
-    calGaussianKernel(data,r);
+    int size=3;
+    float* data=new float[size*size];
+    double sigma=0.84089642;
+    calGaussianKernel(data,size,sigma);
+    for (int i = 0; i < size*size; ++i) {
+        if(i%size==0){
+            cout<<endl;
+        }
+        cout<<data[i]<<" ";
+
+    }
     delete []data;
     return 0;
 }
